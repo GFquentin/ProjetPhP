b03c2d87bcbc86698b5d9a27c84e96126ad09a3e
Anthony SANCHEZ <sanchez.antho@free.fr>
2017-05-11 09:46:29 +0200

    Update init.sql

--- model/init.sql
+++ model/init.sql
@@ -1,73 +1,73 @@
-CREATE DATABASE projetPHP;
-USE projetPHP;
+CREATE DATABASE projetphp;
+USE projetphp;
 
-CREATE TABLE IF NOT EXISTS task
+CREATE TABLE IF NOT EXISTS projetphp.task
 (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   script VARCHAR(100) NOT NULL
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS host
+CREATE TABLE IF NOT EXISTS projetphp.host
 (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   ip VARCHAR(100) NOT NULL
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS projetphp.user
 (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   login VARCHAR(100) NOT NULL,
   password VARCHAR(255) NOT NULL
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS flow
+CREATE TABLE IF NOT EXISTS projetphp.flow
 (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   exec_time INT NOT NULL,
   userId INT NOT NULL,
   CONSTRAINT flow_userid_fk FOREIGN KEY (userId) REFERENCES user(id)
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS taskflow
+CREATE TABLE IF NOT EXISTS projetphp.taskflow
 (
   taskId INT NOT NULL,
   flowId INT NOT NULL,
   ordertask INT NOT NULL,
   CONSTRAINT taskflow_taskid_fk FOREIGN KEY (taskId) REFERENCES task(id),
   CONSTRAINT taskflow_flowid_fk FOREIGN KEY (flowId) REFERENCES flow(id)
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS flowhost
+CREATE TABLE IF NOT EXISTS projetphp.flowhost
 (
   flowId INT NOT NULL,
-	hostId INT NOT NULL,
+  hostId INT NOT NULL,
   CONSTRAINT flowhost_flowid_fk FOREIGN KEY (flowId) REFERENCES flow(id),
   CONSTRAINT flowhost_hostid_fk FOREIGN KEY (hostId) REFERENCES host(id)
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS flowuser
+CREATE TABLE IF NOT EXISTS projetphp.flowuser
 (
-	flowId INT NOT NULL,
-	userId INT NOT NULL,
+  flowId INT NOT NULL,
+  userId INT NOT NULL,
   CONSTRAINT flowuser_flowid_fk FOREIGN KEY (flowId) REFERENCES flow(id),
   CONSTRAINT flowuser_userid_fk FOREIGN KEY (userId) REFERENCES user(id)
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS taskexecution
+CREATE TABLE IF NOT EXISTS projetphp.taskexecution
 (
-	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	taskId INT NOT NULL,
-	starttime DATETIME NOT NULL,
-	status INT NOT NULL
-);
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  taskId INT NOT NULL,
+  starttime DATETIME NOT NULL,
+  status INT NOT NULL
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS flowexecution
+CREATE TABLE IF NOT EXISTS projetphp.flowexecution
 (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   flowId INT NOT NULL,
   starttime DATETIME NOT NULL,
   status INT NOT NULL,
   CONSTRAINT flowexecution_flowid_fk FOREIGN KEY (flowId) REFERENCES flow(id)
-);
+) ENGINE=InnoDB;
