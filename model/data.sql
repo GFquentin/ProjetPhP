cdbcb88c0b11c4ee016c40b3bc611a0c9f4eb830
Anthony SANCHEZ <sanchez.antho@free.fr>
2017-05-12 08:52:23 +0200

    Update data.sql

--- model/data.sql
+++ model/data.sql
@@ -1,13 +1,13 @@
-INSERT INTO projetphp.task (script) VALUES ('/opt/test.sh');
+INSERT INTO task (script) VALUES ('/opt/test.sh');
 
-INSERT INTO projetphp.host (name, ip) VALUES ('debian-anthony', '192.168.14.224');
+INSERT INTO host (name, ip) VALUES ('debian-anthony', '192.168.14.224');
 
-INSERT INTO projetphp.user (login, password) VALUES ('job', 'job');
+INSERT INTO user (login, password) VALUES ('job', 'job');
 
-INSERT INTO projetphp.flow (name, exec_time, userId) VALUES ('flow1', '120', 0);
+INSERT INTO flow (name, exec_time, userId) VALUES ('flow1', '120', 1);
 
-INSERT INTO projetphp.taskflow (taskId, flowId, ordertask) VALUES (0, 0, 1);
+INSERT INTO taskflow (taskId, flowId, ordertask) VALUES (1, 1, 1);
 
-INSERT INTO projetphp.flowhost (flowId, hostId) VALUES (0, 0);
+INSERT INTO flowhost (flowId, hostId) VALUES (1, 1);
 
-INSERT INTO projetphp.flowuser (flowId, userId) VALUES (0, 0);
+INSERT INTO flowuser (flowId, userId) VALUES (1, 1);
