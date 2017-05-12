0ec61ea7e855b86fc44a71c88feaec6bf8e6a9fb
Anthony SANCHEZ <sanchez.antho@free.fr>
2017-05-12 14:40:46 +0200

    Create task.php

---
+++ model/task.php
@@ -0,0 +1,28 @@
+<?php
+
+class Task
+{
+    //<editor-fold desc="Properties">
+    private $id;
+    private $script;
+    //</editor-fold>
+
+    //<editor-fold desc="Constructor">
+    public function __construct($id, $script)
+    {
+        $this->id=$id;
+        $this->script=$script;
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Methods">
+    public function run()
+    {
+
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Functions">
+    //</editor-fold>
+}
+?>
