7f46855966dfcf730521eb38aaf93410854620d5
Anthony SANCHEZ <sanchez.antho@free.fr>
2017-06-01 09:24:36 +0200

    Add files via upload

---
+++ model/user.php
@@ -0,0 +1,44 @@
+<?php
+
+class User
+{
+    //<editor-fold desc="Properties">
+    private $id;
+    private $login;
+    private $password;
+    //</editor-fold>
+
+    //<editor-fold desc="Constructor">
+    public function __construct($id, $login, $password)
+    {
+        $this->id=$id;
+        $this->login=$login;
+        $this->password=$password;
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Methods">
+    //</editor-fold>
+
+    //<editor-fold desc="Functions">
+    //</editor-fold>
+
+    //<editor-fold desc="Getters & Setters">
+    public function getId()
+    {
+
+    }
+
+    public function getLogin()
+    {
+
+    }
+
+    public function getPassword()
+    {
+
+    }
+    //</editor-fold>
+}
+
+?>
